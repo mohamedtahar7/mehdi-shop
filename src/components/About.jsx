397e22b9837bd31afd6924f2db97d6d94374f261
mohamedtahar7 <taharatallahx7@gmail.com>
2023-07-20 16:40:00 +0100

    about img

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <div className='py-36 px-20'>
         <h1 className='text-[#11334f] sm:text-5xl text-center text-3xl'>A Propos de UniConfort</h1>
         <div className='py-24 flex items-center justify-center gap-16 md:flex-row flex-col'>
-          <img className='sm:w-[40%] w-full rounded-xl bg-[#d9dbda] py-16 px-8' src="/src/assets/about-logo.png" alt="logo" />
+          <img className='sm:w-[40%] w-full rounded-xl bg-[#d9dbda] py-16 px-8' src="https://i.postimg.cc/fbYMh4Pm/about-logo.png" alt="logo" />
           <p className='sm:text-3xl text-xl sm:leading-[4rem] leading-[3rem] text-[#11334f] '>UniConfort Fabricant algérien de Mobiliers, depuis 1996. C’est une entreprise algérienne, spécialisés en aménagement des Hôtels, Restaurants et Collectivités.</p>
         </div>
       </div>
