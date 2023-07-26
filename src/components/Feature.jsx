bdeef852581c0146ac3068ae59d61eaf3ccc4d2d
mohamedtahar7 <taharatallahx7@gmail.com>
2023-07-26 16:36:43 +0100

    msg

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -2,17 +2,16 @@ import React from 'react'
 import FeatureCard from './FeatureCard'
 import {FiGrid} from 'react-icons/fi'
 import {BiSolidCheckShield} from 'react-icons/bi'
-import {TbTruckDelivery} from 'react-icons/tb'
+import {RiCustomerService2Line} from 'react-icons/ri'
 import {CiDeliveryTruck} from 'react-icons/ci'
-import {AiFillCustomerService} from 'react-icons/ai'
 const Feature = () => {
   return (
     <div className='px-20  bg-[#11334f]'>
-        <div className='grid place-items-center lg:grid-cols-4 sm:grid-cols-3 grid-cols-1  gap-12 py-12'>
-            <FeatureCard  icon={<FiGrid size={45} className='text-[#fff]'/>} name={'Large sélection de produits'}/>
-            <FeatureCard icon={<BiSolidCheckShield size={55} className='text-[#fff]'/>} name={'Assurance qualité'}/>
+        <div className='grid place-items-center lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 grid-cols-1  gap-12 py-12'>
+            <FeatureCard  icon={<FiGrid size={45} className='text-[#fff]'/>} name={'Une grande sélection de produits'}/>
+            <FeatureCard icon={<BiSolidCheckShield size={55} className='text-[#fff]'/>} name={'Produit de qualité'}/>
             <FeatureCard icon={<CiDeliveryTruck size={55} className='text-[#fff]'/>} name={'Livraison rapide et fiable'}/>
-            <FeatureCard  icon={<AiFillCustomerService size={55} className='text-[#fff]'/>} name={'Service client expert'}/>
+            <FeatureCard  icon={<RiCustomerService2Line size={55} className='text-[#fff]'/>} name={'Service client expert'}/>
         </div>
     </div>
   )
--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const FeatureCard = ({name,description,icon}) => {
   return (
     <div className='w-[80%]'>
-        <div className='flex flex-col gap-4'>
+        <div className='flex flex-col gap-2'>
           {icon}
           <h4 className='text-[#fff] lg:text-2xl text-xl font-normal'>{name}</h4>
         </div>  
