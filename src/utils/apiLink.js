9583274c6ee45dae4d7dc758387631380039ac79
Mohamed Tahar Atallah <120719194+mohamedtahar7@users.noreply.github.com>
2024-05-25 00:06:00 +0200

    Update apiLink.js

--- src/utils/apiLink.js
+++ src/utils/apiLink.js
@@ -1,3 +1,3 @@
 /*export const apiLink = "https://faithful-panama-hat-crow.cyclic.cloud";
 */
-export const apiLink = "https://mehdi-api.onrender.com";
+export const apiLink = "https://mehdi-api.vercel.app/";
